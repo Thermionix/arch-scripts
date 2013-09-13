3f0ad4e813f6b8dae08972f0cbf6a8a18ed0f87e
Thermionix <bond711@gmail.com>
2013-09-13 20:41:50 +1000

    Update arch-desktop.sh

--- arch-desktop.sh
+++ arch-desktop.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
  
-pacman -S xorg-server xorg-server-utils xorg-xinit mesa
+sudo pacman -S xorg-server xorg-server-utils xorg-xinit mesa
  
-pacman -S ttf-droid ttf-liberation ttf-dejavu xorg-fonts-type1
-ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/
+sudo pacman -S ttf-droid ttf-liberation ttf-dejavu xorg-fonts-type1
+sudo ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/
  
 #pacman -S xf86-video-vesa
  
 #pacman -S mate mate-extras
 #echo "exec mate-session" > ~/.xinitrc
  
-pacman -S firefox vlc p7zip clementine gstreamer0.10-plugins flashplugin
+sudo pacman -S firefox vlc p7zip clementine gstreamer0.10-plugins flashplugin
  
-pacman -S openssh ntfsprogs rsync
+sudo pacman -S openssh ntfsprogs rsync
  
-pacman -S mumble steam
+sudo pacman -S mumble steam
  
 echo "complete -cf sudo" >> ~/.bashrc
  
