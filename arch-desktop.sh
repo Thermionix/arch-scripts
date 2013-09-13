175aa283242f554bc06924f5b5fa6c2c46d58c35
Thermionix <bond711@gmail.com>
2013-09-13 20:45:50 +1000

    Update arch-desktop.sh

--- arch-desktop.sh
+++ arch-desktop.sh
@@ -15,15 +15,20 @@ sudo pacman -S firefox vlc p7zip clementine gstreamer0.10-plugins flashplugin
 sudo pacman -S openssh ntfsprogs rsync
  
 sudo pacman -S mumble steam
- 
-echo "complete -cf sudo" >> ~/.bashrc
+
+if [ `grep "complete -cf sudo" ~/.bashrc` -ne 0 ]; then 
+  echo "complete -cf sudo" >> ~/.bashrc
+fi
  
 #systemctl disable dhcpcd
 #systemctl stop dhcpcd
 #systemctl enable NetworkManager
 #systemctl start NetworkManager
- 
-echo "## the next command will add startx after login to .bash_profile"
-read -p "Press [Enter] key to continue"
-test -f /home/$username/.bash_profile || cp /etc/skel/.bash_profile ~/.bash_profile
-echo "[[ -z \$DISPLAY && \$XDG_VTNR -eq 1 ]] && exec startx" >> ~/.bash_profile
+
+
+if [ `grep "exec startx" ~/.bash_profile` -ne 0 ]; then 
+  echo "## the next command will add startx after login to .bash_profile"
+  read -p "Press [Enter] key to continue"
+  test -f /home/$username/.bash_profile || cp /etc/skel/.bash_profile ~/.bash_profile
+  echo "[[ -z \$DISPLAY && \$XDG_VTNR -eq 1 ]] && exec startx" >> ~/.bash_profile
+fi
