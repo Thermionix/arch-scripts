ecc635a9b96365eb59013ddfb6fbb50a00ad47e4
Thermionix <bond711@gmail.com>
2013-09-13 20:47:51 +1000

    Update arch-wine.sh

--- arch-wine.sh
+++ arch-wine.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
  
 if [[ `uname -m` == x86_64 ]]; then
-echo "## x86_64 detected, adding multilib repository"
-if [[ -z `grep -n "\[multilib\]" /etc/pacman.conf | cut -f1 -d:` ]]; then
-echo -e "\n[multilib]\nInclude = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
-else
-sed -i '/#\[multilib\]/,/#Include = \/etc\/pacman.d\/mirrorlist/ s/#//' /etc/pacman.conf
+  echo "## x86_64 detected, adding multilib repository"
+  if [[ -z `grep -n "\[multilib\]" /etc/pacman.conf | cut -f1 -d:` ]]; then
+    echo -e "\n[multilib]\nInclude = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf
+  else
+    sudo sed -i '/#\[multilib\]/,/#Include = \/etc\/pacman.d\/mirrorlist/ s/#//' /etc/pacman.conf
+  fi
 fi
-fi
-pacman -Syy
+sudo pacman -Syy
  
 #unset XMODIFIERS && export SDL_AUDIODRIVER=alsa && steam
  
-pacman -S lib32-alsa-lib wine winetricks wine-mono wine_gecko
+sudo pacman -S lib32-alsa-lib wine winetricks wine-mono wine_gecko
  
 #winetricks videomemorysize=2048 3072?
 WINEARCH=win32 winecfg
