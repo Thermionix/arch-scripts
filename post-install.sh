d7db20f2f8b6d578993df0c2c2b8aaacb134aaf6
Thermionix <bond711@gmail.com>
2013-09-16 20:03:58 +1000

    new file:   post-install.sh

---
+++ post-install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#packer
+
+#arch-desktop.sh
+
+#video driver
+#pacman -S xf86-video-vesa
+# virtualbox
+#intel
+#catalyst.sh
+
+#DE
+
+#MATE
+#KDE
+#GNOME-flashback
+
+
+#enable auto login to x?
+
+#wine
+
+# pacman -S network-manager-applet networkmanager
+# systemctl enable NetworkManager
+# systemctl start NetworkManager
+
+# pacman -S pulseaudio pulseaudio-alsa pavucontrol
+
+# pacman -S cups cups-filters foomatic-filters ghostscript gsfonts system-config-printer
+# systemctl enable cups
+# systemctl start cups
