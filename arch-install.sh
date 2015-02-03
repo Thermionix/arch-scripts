469363fe8aaa2dc094a2b6ad0b800154260456bd
Thermionix <Thermionix@users.noreply.github.com>
2015-02-03 16:59:15 +1100

    fix missing build tools

--- arch-install.sh
+++ arch-install.sh
@@ -187,7 +187,8 @@ format_disk() {
 	if $enable_bcache ; then
 		pacman -Sy --noconfirm git
 		# TODO : don't fail if nothing to install
-		fgrep -vf <(pacman -Qq) <(pacman -Sgq base-devel) | xargs pacman -S --noconfirm
+		#fgrep -vf <(pacman -Qq) <(pacman -Sgq base-devel) | xargs pacman -S --noconfirm
+		pacman -S --noconfirm base-devel
 		export EDITOR=nano
 		curl https://aur.archlinux.org/packages/bc/bcache-tools/bcache-tools.tar.gz | tar -zx --directory=/tmp
 		pushd /tmp/bcache-tools
